d07c2c87fb13f63cf98a4af498d0867fa5643c0c
qiusen <2559231099@qq.com>
2024-06-22 18:02:43 +0800

    初始化dev_02代码

--- src/main/java/com/jzo2o/foundations/service/impl/ServeItemServiceImpl.java
+++ src/main/java/com/jzo2o/foundations/service/impl/ServeItemServiceImpl.java
@@ -48,10 +48,8 @@ public class ServeItemServiceImpl extends ServiceImpl<ServeItemMapper, ServeItem
 
     @Resource
     private ServeTypeMapper serveTypeMapper;
-
     @Resource
-    private IServeService iServeService;
-
+    private IServeService serveService;
     /**
      * 服务项新增
      *
@@ -165,9 +163,10 @@ public class ServeItemServiceImpl extends ServiceImpl<ServeItemMapper, ServeItem
             throw new ForbiddenOperationException("启用状态方可禁用");
         }
 
-        int count = iServeService.queryServeCountByServerItemAndSaleStatus(id, FoundationStatusEnum.ENABLE.getStatus());
+        //有区域在使用该服务将无法禁用（存在关联的区域服务且状态为上架表示有区域在使用该服务项）
+        int count = serveService.queryServeCountByServeItemIdAndSaleStatus(id, FoundationStatusEnum.ENABLE.getStatus());
         if (count > 0) {
-            throw new ForbiddenOperationException("区域下有上架的服务无法禁用");
+            throw new ForbiddenOperationException("该服务有区域正在使用，无法进行禁用,请先将区域内的服务下架。");
         }
 
         //更新禁用状态
